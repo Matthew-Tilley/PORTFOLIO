418945888e21e57de158ee1ef6ca27081d42bb4e
Matt Tilley <mtilley255@gmail.com>
2017-11-18 14:31:55 -0600

    Just a couple changes - release the GitKraken!!!  :)

--- JS/primary.js
+++ JS/primary.js
@@ -1,27 +1,98 @@
 $(document).ready(function()
 {
 
+    
     $("#BTN01").on("click", function()      /*OPENS #SEC03*/
     {
-        $("#SEC03").show();
+        $(".LOCT11").hide();
+        $("#SEC03").show();      
+        
+             
+        $("#SEC03").on("click", function()      /*CLOSES #SEC03*/
+        {
+            $("#SEC03").hide();
+            $(".LOCT11").show();
+        });
+        
     });
     
-    $("#SEC03").on("click", function()      /*CLOSES #SEC03*/
+   
+     
+    $("#BTN02").on("click", function()      /*OPENS SEC04*/
     {
-        $("#SEC03").hide();
-    });
-    
-    
-    
-    $("#BTN02").on("click", function()
-    {
+        $(".LOCT11").hide();
         $("#SEC04").show();
         
-        $("#SEC04").on("click", function()
+
+        $("#BTN03").on("click", function()   /*CLOSES SEC04*/
         {
             $("#SEC04").hide();
+            $(".LOCT11").show();
+            
         });
+      
+    
+    
+        
+        /* SHOWS MODAL ON HOVER - DO NOT DELETE */
+        /* $(".ICNT01").hover(function()   
+        {
+
+            var a = $(this).attr("id") + "-A";
+
+            var b = document.getElementById(a);
+
+            $(b).show();
+
+            $(b).mouseleave(function()
+            {
+                $(b).hide();
+            });
+            
+        });  */
+        
+        
+        /* THIS IS THE ABOVE CODE - JUST WITH A CLICK EVENT HANDLER INSTEAD */
+        $(".ICNT01").on("click", function()
+        {
+            var a = $(this).attr("id") + "-A";
+
+            console.log(a);
+
+            var b = document.getElementById(a);
+
+            $(b).show(function()
+            {
+                $(b).on("click", function()
+                {
+                    $(this).hide();
+                });
+            });
+            
+        });
+        
+
+        
+
+
     });
+    
+    
+    
+  
+    
+   
+    
+   
+    
+    /*$("#IMG-DW01").show();     */
+    /*MAKE THIS INTO A CYCLING FADER*/
+    /*MAKE THIS INTO A CYCLING FADER*/
+    /*MAKE THIS INTO A CYCLING FADER*/
+    
+    
   
     
 });
+
+
